11f13e04775b8bf8987f26329c47484efe05a84a
nellizi <masterliz9095@gmail.com>
2023-05-12 11:05:40 +0900

    Refactor : .jar 파일 추가 , mvn dependency 추가

--- src/main/java/com/example/test/AppRepository.java
+++ src/main/java/com/example/test/AppRepository.java
@@ -1,7 +1,5 @@
 package com.example.test;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.QueryByExampleExecutor;
 import org.springframework.stereotype.Repository;
